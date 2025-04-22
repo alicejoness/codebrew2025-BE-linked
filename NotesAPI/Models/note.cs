ef2c63c5da416f89dc0565d77c24c52c085aa441
aishwaryshreecissa <aishwary.shree@cissa.org.au>
2025-04-22 15:13:23 +1000

    Added documentation comments to note.cs

--- NotesAPI/Models/note.cs
+++ NotesAPI/Models/note.cs
@@ -5,16 +5,20 @@ namespace NotesAPI.Models
 {
     public class Note
     {
+        // MongoDB ObjectId used as the unique ID
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string? Id { get; set; }
 
+        // Title of the note
         [BsonElement("title")]
         public string? Title { get; set; }
 
+        // Content/body of the note
         [BsonElement("content")]
         public string? Content { get; set; }
 
+        // Timestamp for when the note was created
         [BsonElement("createdAt")]
         public DateTime CreatedAt { get; set; } = DateTime.Now;
     }
